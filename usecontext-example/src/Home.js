1183f97e07a891a85e11ee5ca730fecbb296ab6a
oliver <oliver.wu.1987@gmail.com>
2021-01-28 04:39:45 +0000

    add context comsumer

--- usecontext-example/src/Home.js
+++ usecontext-example/src/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
+import LocaleContext from './LocaleContext'
 
 const Home = () => {
-  return <>Home</>;
+	return (
+		<LocaleContext.Consumer>
+			{
+				(value) => {
+					const { locale } = value
+					return (
+						<>
+						{ locale === 'en' && <h1>Home En</h1>}
+						{ locale === 'es' && <h1>Home Es</h1>}
+						</>
+					)
+				}
+			}
+		</LocaleContext.Consumer>
+	);
 };
 
 export default Home;
