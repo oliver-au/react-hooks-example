87c5f3433501f584caba72c48aee229e9ede9e58
oliver <oliver.wu.1987@gmail.com>
2021-01-28 09:34:09 +0000

    add react.memo example

---
+++ react-memo-example/src/components/NthPrime.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { calculatePrime, suffixOf } from './math'
+
+const NthPrime = ({ count, increment }) => {
+  const prime = calculatePrime(count)
+
+  return (
+    <div className='container'>
+      <h2>Nth Prime</h2>
+      <p>
+        The <b>{suffixOf(count)}</b> prime number is <b>{prime}</b>.
+      </p>
+      <button onClick={increment}>Next prime</button>
+    </div>
+  )
+}
+
+export default React.memo(NthPrime, (prevProps, nextProsp) => {
+	return prevProps.count === nextProsp.count
+})
